c544967de874bb50c225148bf74fc21a621a6811
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-24 21:29:10 -0300

    [Modelando o Domínio] Optional Parameters

--- UtmBuilder/UtmBuilder.Core/ValueObjects/Campaign.cs
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -2,10 +2,27 @@
 
 public class Campaign : ValueObject
 {
-    public string Id { get; set; }
-    public string Source { get; set; }
-    public string Medium { get; set; }
-    public string Name { get; set; }
-    public string Term { get; set; }
-    public string Content { get; set; }
+    public Campaign(
+        string source,
+        string medium,
+        string name,
+        string? id = null,
+        string? term = null,
+        string? content = null)
+    {
+        Source = source;
+        Medium = medium;
+        Name = name;
+        Id = id;
+        Term = term;
+        Content = content;
+    }
+
+    public string Source { get; }
+    public string Medium { get; }
+    public string Name { get; }
+
+    public string? Id { get; }
+    public string? Term { get; }
+    public string? Content { get; }
 }

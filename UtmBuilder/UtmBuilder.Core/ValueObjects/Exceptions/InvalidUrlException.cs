a0a218474c0a4dd1dc6d1acf5a0d74d14876d4de
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-25 19:58:22 -0300

    [Modelando o Domínio] Exceptions

---
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -0,0 +1,10 @@
+﻿namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public class InvalidUrlException : Exception
+{
+    public InvalidUrlException(string message = "Invalid URL")
+        : base(message)
+    {
+        
+    }
+}
--- UtmBuilder/UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.ValueObjects;
 
@@ -15,8 +16,8 @@ public class Url : ValueObject
     {
         Address = address;
 
-        if (Regex.IsMatch(Address, UrlRegexPattern))
-            throw new Exception("Teste");
+        if (Regex.IsMatch(Address, UrlRegexPattern))            
+            throw new InvalidUrlException();
     }
 
     /// <summary>

c446c8e17955d254514111d0109faf808c762e55
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-27 08:24:25 -0300

    [Modelando o Domínio] Estendendo uma lista

--- UtmBuilder/UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder/UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -2,8 +2,12 @@
 
 public static class ListExtensions
 {
-    public static void AddIfNotNull(this List<string> list)
+    public static void AddIfNotNull(
+        this List<string> list,
+        string key,
+        string? value)
     {
-
+        if (!string.IsNullOrEmpty(value))
+            list.Add($"{key}={value}");
     }
 }

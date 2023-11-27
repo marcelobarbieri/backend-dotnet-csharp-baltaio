4985b63f3186b5ceeb019c8996c18226e4347ac7
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-27 07:21:42 -0300

    [Modelando o Domínio] Source Generators

--- UtmBuilder/UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder/UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -3,11 +3,9 @@ using System.Text.RegularExpressions;
 
 namespace UtmBuilder.Core.ValueObjects.Exceptions;
 
-public class InvalidUrlException : Exception
+public partial class InvalidUrlException : Exception
 {
     private const string DefaultErrorMessage = "Invalid URL";
-    private const string UrlRegexPattern =
-    @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
 
     public InvalidUrlException(string message = DefaultErrorMessage)
         : base(message)
@@ -22,7 +20,10 @@ public class InvalidUrlException : Exception
         if (string.IsNullOrEmpty(address))
             throw new InvalidUrlException(message);
 
-        if (!Regex.IsMatch(address, UrlRegexPattern))
+        if (!UrlRegex().IsMatch(address))
             throw new InvalidUrlException(message);
     }
+
+    [GeneratedRegex("^(http|https):(\\\\/\\\\/www\\\\.|\\\\/\\\\/www\\\\.|\\\\/\\\\/|\\\\/\\\\/)[a-z0-9]+([\\\\-\\\\.]{1}[a-z0-9]+)*\\\\.[a-z]{2,5}(:[0-9]{1,5})?(\\\\/.*)?$|(http|https):(\\\\/\\\\/localhost:\\\\d*|\\\\/\\\\/127\\\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\\\/.*)?$")]
+    private static partial Regex UrlRegex();
 }

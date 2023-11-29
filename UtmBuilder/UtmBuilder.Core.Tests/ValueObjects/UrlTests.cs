a53a1bdc1c9e733333342b620d5723310c3ae217
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-28 21:25:09 -0300

    [Testes de Unidade] Testando por exceções

--- UtmBuilder/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -1,14 +1,30 @@
-﻿namespace UtmBuilder.Core.Tests.ValueObjects;
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
 
 [TestClass]
 public class UrlTests
 {
     [TestMethod("Deve retornar uma exceção quando a Url for inválida")]
     [TestCategory("Teste de Url")]
-    //public void Dada_uma_url_invalida_deve_retornar_uma_excecao()
-    //public void DadaUmaUrlInvalidaDeveRetornarUmaExcecao()
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
+        try
+        {
+            var url = new Url("banana");
+            Assert.Fail();
+        }
+        catch (InvalidUrlException e)
+        {
+            Assert.IsTrue(true);
+        }
+    }
 
+    [TestMethod("Não deve retornar uma exceção quando a Url for válida")]
+    [TestCategory("Teste de Url")]
+    public void ShouldNotReturnExceptionWhenUrlIsValid()
+    {
+        Assert.Fail();
     }
 }

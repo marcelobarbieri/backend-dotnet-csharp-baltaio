7f120e439ebb3965bf9030fdf873606e1a1c1377
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-29 22:15:17 -0300

    [Testes de Unidade] DataRow

--- UtmBuilder/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -6,19 +6,51 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 [TestClass]
 public class UrlTests
 {
+    private const string InvalidUrl = "banana";
+    private const string ValidUrl = "https://balta.io";
+
     [TestMethod("Deve retornar uma exceção quando a Url for inválida")]
     [TestCategory("Teste de Url")]
     [ExpectedException(typeof(InvalidUrlException))]
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
-        new Url("banana");
+        new Url(InvalidUrl);
     }
 
     [TestMethod("Não deve retornar uma exceção quando a Url for válida")]
     [TestCategory("Teste de Url")]
     public void ShouldNotReturnExceptionWhenUrlIsValid()
     {
-        new Url("https://balta.io");
+        new Url(ValidUrl);
         Assert.IsTrue(true);
+    }
+
+    [TestMethod("Teste de Url (DataRow)")]
+    [TestCategory("Teste de Url")]
+    [DataRow(" ", true)]
+    [DataRow("http", true)]
+    [DataRow("banana", true)]
+    [DataRow("https://balta.io",false)]
+    public void TestUrl(
+        string link, 
+        bool expectException)
+    {
+        if (expectException)
+        {
+            try
+            {
+                new Url(link);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        else
+        {
+            new Url(link);
+            Assert.IsTrue(true);
+        }
     }
 }

4a47e0378d619bc7d7c8b348d6d733bb6387147b
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-11-30 22:43:54 -0300

    [Testes de Unidade] Testando os Implicit Operators

--- UtmBuilder/UtmBuilder.Core.Tests/UtmTests.cs
+++ UtmBuilder/UtmBuilder.Core.Tests/UtmTests.cs
@@ -5,21 +5,44 @@ namespace UtmBuilder.Core.Tests;
 [TestClass]
 public class UtmTests
 {
-    [TestMethod("Deve retornar uma Url de UTM")]
-    [TestCategory("Teste de UTM")]
+    private const string Result = "https://balta.io/" +
+                                  "?utm_source=src" +
+                                  "&utm_medium=med" +
+                                  "&utm_campaign=nme" +
+                                  "&utm_id=id" +
+                                  "&utm_term=ter" +
+                                  "&utm_content=ctn";
+
+    private readonly Url _url = new("https://balta.io/"); // target type, omite a classe antes do new
+    private readonly Campaign _campaign =  new( // target type, omite a classe antes do new
+        source: "src", 
+        medium: "med", 
+        name: "nme", 
+        id: "id", 
+        term: "ter", 
+        content: "ctn");
+
+    [TestMethod("Deve retornar uma Url de Utm")]
+    [TestCategory("Teste de Utm")]
     public void ShouldReturnUrlFromUtm()
     {
-        var url = new Url("https://balta.io/");
-        var cmp = new Campaign("src","med","nme","id","ter","ctn");
-        var utm = new Utm(url,cmp);
+        var utm = new Utm(_url,_campaign);
 
-        var result = "https://balta.io/" +
-                     "?utm_source=src" +
-                     "&utm_medium=med" +
-                     "&utm_campaign=nme" +
-                     "&utm_id=id" +
-                     "&utm_term=ter" +
-                     "&utm_content=ctn";
-        Assert.AreEqual(result, utm.ToString());
+        Assert.AreEqual(Result, utm.ToString());
+        Assert.AreEqual(Result, (string)utm);
+    }
+
+    [TestMethod("Deve retornar um Utm da Url")]
+    [TestCategory("Teste de Utm")]
+    public void ShouldReturnUtmFromUrl()
+    {
+        Utm utm = Result;
+        Assert.AreEqual("https://balta.io/", utm.Url.Address);
+        Assert.AreEqual("src", utm.Campaign.Source);
+        Assert.AreEqual("med", utm.Campaign.Medium);
+        Assert.AreEqual("nme", utm.Campaign.Name);
+        Assert.AreEqual("id", utm.Campaign.Id);
+        Assert.AreEqual("ter", utm.Campaign.Term);
+        Assert.AreEqual("ctn", utm.Campaign.Content);
     }
 }
